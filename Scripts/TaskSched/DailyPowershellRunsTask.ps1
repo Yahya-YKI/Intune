91c754d92372b9a07c5e8e6a389f48d90e82882d
Yahya-YKI <yahya.kouzi@issroad.com>
2024-05-10 16:22:28 +0100

    Add TaskSched
    This TaskSched ensures that all defined powershell files will run at a specified time.

---
+++ Scripts/TaskSched/DailyPowershellRunsTask.ps1
@@ -0,0 +1,58 @@
+################################################################################
+#                                                                              #
+#   Author: YKI                                                                #
+#   Date: 10-05-2024                                                           #
+#   Script: RegisterDailyTask.ps1                                              #
+#   Purpose: This script registers a daily task in Windows Task Scheduler to   #
+#            execute the DailyTasks.ps1 script located in the                  #
+#            C:\ISSROAD\Intune\Scripts\TaskSched directory.                    #
+#            The DailyTasks.ps1 has all the powershell files to run            #
+#            The task is configured using the taskSched.xml template file.     #
+#            If a task with the same name already exists, the script exits.    #
+#                                                                              #
+################################################################################
+
+
+# Logs Vars
+$appName = $MyInvocation.MyCommand.Name -replace '\.ps1$'
+$currentDate = Get-Date
+$logpath = "C:\Logs\$appName"
+$logfile = "$logpath\log__"+$currentDate.ToString("dd-MM-yyyy__HH-mm")+".txt"
+New-Item -ItemType Directory -Path $logpath -Force
+
+# Script Vars
+$previousLocation = Get-Location
+$workingPath = "C:\ISSROAD\Intune\Scripts\TaskSched"
+
+Set-Location $workingPath # "C:\ISSROAD\Intune\Scripts\TaskSched"
+
+# Check if the task already exists
+$existingTask = Get-ScheduledTask -TaskName $appName -ErrorAction SilentlyContinue
+if ($null -ne $existingTask) {
+    Write-Output "Task '$appName' already exists. Exiting." | Out-File -FilePath $logfile -Append
+    Set-Location $previousLocation
+    Exit
+}
+
+
+# Define the path to the XML file
+$xmlFilePath = "taskSched.xml"
+
+# Read the XML content
+$xmlContent = Get-Content -Path $xmlFilePath
+
+# Define the full path to the script
+$scriptPath = "$workingPath\DailyTasks.ps1"
+
+# Define the arguments for the script
+$arguments = "-ExecutionPolicy Bypass -File `"$scriptPath`""
+
+# Replace <Arguments> and <URI> in the XML content
+$xmlContent = $xmlContent -replace '<Arguments>.*</Arguments>', "<Arguments>$arguments</Arguments>"
+$xmlContent = $xmlContent -replace '<URI>.*</URI>', "<URI>$scriptPath</URI>"
+
+# Convert the XML content to a string
+$xmlString = $xmlContent | Out-String
+
+# Register the task in Task Scheduler
+Register-ScheduledTask -Xml $xmlString -TaskName $appName -Force
