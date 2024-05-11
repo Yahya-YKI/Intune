e56a4b12045a331bab631ade7bd413cb18706dad
Yahya-YKI <yahya.kouzi@issroad.com>
2024-05-11 21:31:01 +0100

    added scriptname sanitization function

--- Scripts/TaskSched/DailyTasks.ps1
+++ Scripts/TaskSched/DailyTasks.ps1
@@ -13,6 +13,29 @@ New-Item -ItemType Directory -Path $logpath -Force
 $result = git -C "C:\ISSROAD\Intune\" pull 2>&1
 Write-Output "Result of Git Pull : $result" | Out-File -FilePath $logfile -Append
 
+# A function that sanitizes the input scripts name imported from ScriptsToRun.txt file
+function Sanitize {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$ScriptName
+    )
+    # Check if the string doesn't end with ".ps1", and if so, append it
+    $oldName =$ScriptName
+    if (-not $ScriptName.EndsWith(".ps1")) {
+        $ScriptName += ".ps1"
+
+    }
+    # Remove "\" or "/" characters from the string
+    $ScriptName = $ScriptName -replace '[\\/\:]', ''
+
+    if (-not ($oldName -eq $ScriptName)) {
+        Write-Output "Warning : ScriptName ""$oldName"" has been sanitized to ""$ScriptName""" | Out-File -FilePath $logfile -Append
+        Write-Output "Please input the filename with extension (ps1) only. no path is needed" | Out-File -FilePath $logfile -Append
+    }
+    
+    return $ScriptName
+}
+
 $previousLocation = Get-Location
 $workingPath = "C:\ISSROAD\Intune"
 
@@ -21,7 +44,7 @@ $scriptsToExecutePath = "Scripts\TaskSched\ScriptsToRun.txt"
 
 $scriptsToExecute = Get-Content -Path $scriptsToExecutePath | Where-Object { $_ -notmatch '^\s*(#|$)' }
 foreach ($script in $scriptsToExecute) {
-    $matchingFile = Get-ChildItem -Path . -Recurse -Filter $script
+    $matchingFile = Get-ChildItem -Path . -Recurse -Filter (Sanitize -ScriptName $script)
     if ($matchingFile) {
         # Get the full path of the file
         $fullPath = $matchingFile.FullName
---
+++ ToRemove.ps1
@@ -0,0 +1,18 @@
+# Start the timer
+$startTime = Get-Date
+
+# Your script code here
+# For example, let's wait for a few seconds to simulate script execution
+Start-Sleep -Seconds 3
+
+# Calculate the duration of running
+$duration = ((Get-Date) - $startTime).TotalSeconds
+
+# Generate the content for the text file
+$content = "Script ran successfully in $duration"
+
+# Specify the path to the output file
+$outputFile = "C:\Test_script.txt"
+
+# Write the content to the output file
+$content | Set-Content -Path $outputFile
