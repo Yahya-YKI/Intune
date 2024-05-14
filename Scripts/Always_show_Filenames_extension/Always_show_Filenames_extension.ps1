0c86d634d3b2f8443d751bb7ac2ec80a77208382
Yahya-YKI <yahya.kouzi@issroad.com>
2024-05-14 16:04:05 +0100

    Update Always_show_Filenames_extension.ps1

--- Scripts/Always_show_Filenames_extension/Always_show_Filenames_extension.ps1
+++ Scripts/Always_show_Filenames_extension/Always_show_Filenames_extension.ps1
@@ -1,16 +1,27 @@
-﻿$appName = $MyInvocation.MyCommand.Name -replace '\.ps1$'
-# Function to add missing registry key
-function AddMissingRegistryKey($registryPath) {
-    New-Item -Path $registryPath -Force | Out-Null
-    Set-ItemProperty -Path $newKeyPath -Name "HideFileExt" -Value 0
+﻿$previousLocation = Get-Location
+Set-Location -Path $PSScriptRoot
+
+# Global Vars
+$GlobalVarsPath = "..\..\GlobalVars.txt"
+$GlobalVars = @{}
+Get-Content $GlobalVarsPath | ForEach-Object {
+    $variable, $value = ($_ -replace ' = ', '=') -split '='
+    $GlobalVars[$variable] = $value
 }
 
+# Logs Vars
+$appName = $MyInvocation.MyCommand.Name -replace '\.ps1$'
+$currentDate = Get-Date
+$logpath = $GlobalVars['logPath']+"$appName"
+$logfile = "$logpath\log__"+$currentDate.ToString("dd-MM-yyyy__HH-mm")+".txt"
+New-Item -ItemType Directory -Path $logpath -Force
 
 
 # Function to check if registry values match desired values
 function CheckRegistryValues($registryPath) {
     $subkeyValues = Get-ItemProperty -Path "$registryPath"
     if ($subkeyValues.HideFileExt -eq 0) {
+        Write-Output "HideFileExt=0, no further actions needed." | Out-File -FilePath $logfile -Append
         return $true
     }
     return $false
@@ -18,13 +29,18 @@ function CheckRegistryValues($registryPath) {
 
 # Edit Registry and Restart Explorer to show extensions
 if (-not (CheckRegistryValues)) {
+    Write-Output "HideFileExt!=0 or not exists, Setting it to 0." | Out-File -FilePath $logfile -Append
     Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
+    if (CheckRegistryValues) {
+        Write-Output "Registry key has been set to 0." | Out-File -FilePath $logfile -Append
+    }else {
+        Write-Output "Registry modification failed. Trying later" | Out-File -FilePath $logfile -Append
+    }
     Stop-Process -Name explorer -Force
     Start-Process explorer
 }
 
 # Update last execution time
-$currentDate = Get-Date
 $lastExecFile = Join-Path -Path $PSScriptRoot -ChildPath "LastExec.txt"
 Set-Content -Path $lastExecFile -Value $currentDate
 
@@ -45,4 +61,5 @@ if (Test-Path $RegistryPath) {
     # Add a key and set it to the current date
     Set-ItemProperty -Path $RegistryPath -Name $RegistryLastExecuted -Value $currentDate
 }
+Set-Location $previousLocation
 
